cd316f12185989af7d739e1cffe67cdf177c28b0
Daniel Farr <daniel@aptivada.com>
2023-01-04 22:03:09 -0700

    ready to create ship table

---
+++ resources/js/components/Ship/BasicShipForm.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { reactive } from "vue";
+
+const emit = defineEmits(["submit"]);
+
+const props = defineProps({
+    ship: {
+        type: Object,
+        default: null,
+    },
+});
+
+const form = reactive({
+    name: "",
+});
+</script>
+
+<template></template>
