76dc105c41df6167399e1034eaadbaa305ee1876
Daniel Farr <daniel@aptivada.com>
2023-01-05 22:52:00 -0700

    add ability types array

--- resources/js/services/DataService.js
+++ resources/js/services/DataService.js
@@ -12,4 +12,61 @@ export default class DataService {
         ARMOR: "armor",
         UNIT: "unit",
     };
+
+    static getAbilityTypes() {
+        return [
+            {
+                name: "Reduce Damage",
+                slug: "reduce-damage",
+            },
+            {
+                name: "Increase Resistence",
+                slug: "increase-resistence",
+            },
+            {
+                name: "Increase Weapon Damage",
+                slug: "increase-weapon-damage",
+            },
+            {
+                name: "Increase Penetration",
+                slug: "increase-penetration",
+            },
+            {
+                name: "Increase HP",
+                slug: "increase-hp",
+            },
+            {
+                name: "Increase Armor",
+                slug: "increase-armor",
+            },
+            {
+                name: "Increase Accuracy",
+                slug: "increase-accuracy",
+            },
+            {
+                name: "Increase Evade",
+                slug: "increase-evade",
+            },
+            {
+                name: "Increase Shield",
+                slug: "increase-shield",
+            },
+            {
+                name: "Increase Movement Speed",
+                slug: "increase-movement-speed",
+            },
+            {
+                name: "Increase FTL Speed",
+                slug: "increase-ftl-speed",
+            },
+            {
+                name: "Extra Attack",
+                slug: "extra-attack",
+            },
+            {
+                name: "Increase Attack Speed",
+                slug: "increase-attack-speed",
+            },
+        ];
+    }
 }
