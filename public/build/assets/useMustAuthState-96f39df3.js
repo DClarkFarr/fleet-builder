70a94618079e81abff159726d880cb2505ba7bd4
Daniel Farr <daniel@aptivada.com>
2023-01-21 10:53:27 -0700

    stat fixes + prod build

--- public/build/assets/useMustAuthState-96f39df3.js
+++
@@ -1,1 +0,0 @@
-import{u as s,av as e}from"./index-935aeafb.js";const r=()=>{const t=s();e(()=>{!t.isLoading&&!t.user&&(window.location.href="/")})};export{r as u};
---
+++ public/build/assets/useMustAuthState-e6f98085.js
@@ -0,0 +1,1 @@
+import{u as s,av as e}from"./index-474b6c89.js";const r=()=>{const t=s();e(()=>{!t.isLoading&&!t.user&&(window.location.href="/")})};export{r as u};
