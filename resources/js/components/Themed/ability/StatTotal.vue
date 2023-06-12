a43d5a54289bdf3f8b8d98cb362dc604a9f637c5
Daniel Farr <daniel@aptivada.com>
2023-06-11 23:44:19 -0600

    refactor stat total

--- resources/js/components/Themed/ability/StatTotal.vue
+++ resources/js/components/Themed/ability/StatTotal.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { ref } from "vue";
 import IconChip from "~icons/fa-solid/microchip";
 import IconFlag from "~icons/fa-solid/flag";
 import IconExclamationCircle from "~icons/fa-solid/exclamation-circle";
 import IconExclamationTriangle from "~icons/fa-solid/exclamation-triangle";
 import IconChevronCircleDown from "~icons/fa-solid/chevron-circle-down";
 import IconShareAlt from "~icons/fa-solid/share-alt";
+import IconQuestionCircle from "~icons/fa-solid/question-circle";
+import DataService from "../../../services/DataService";
 
 const props = defineProps({
     total: {
@@ -12,6 +15,12 @@ const props = defineProps({
         required: true,
     },
 });
+
+const opened = ref(false);
+
+const toggleOpened = () => {
+    opened.value = !opened.value;
+};
 /*
 var example = {
     type: "increase_armor",
@@ -65,58 +74,90 @@ var example = {
 <template>
     <div class="stat-total-wrap">
         <div
-            class="stat-total text-sm relative border-l-4 flex items-center gap-x-4 leading-none"
+            class="stat-total text-sm relative border-l-4 leading-none"
             :class="[`stat-total--${total.affectType}`]"
         >
-            <div class="stat-total__icons flex gap-x-2">
+            <div class="flex items-center gap-x-4">
                 <div
-                    v-if="total.hasConditions"
-                    class="cursor-pointer"
-                    v-tooltip="
-                        `Condition: ${total.source.extra.conditionsDescription}`
+                    class="stat-total__icons flex gap-x-2"
+                    v-if="
+                        total.hasConditions ||
+                        total.hasQualifiers ||
+                        total.appliesToFleet
                     "
                 >
-                    <IconExclamationCircle class="text-sm text-orange-700" />
+                    <div
+                        v-if="total.hasConditions"
+                        class="cursor-pointer"
+                        v-tooltip="
+                            `Condition: ${total.source.extra.conditionsDescription}`
+                        "
+                    >
+                        <IconExclamationCircle
+                            class="text-sm text-orange-700"
+                        />
+                    </div>
+                    <div
+                        v-if="total.hasQualifiers"
+                        class="cursor-pointer"
+                        v-tooltip="
+                            `Qualifier: ${total.source.extra.abilityQualifiers}`
+                        "
+                    >
+                        <IconExclamationTriangle
+                            class="text-sm text-orange-900"
+                        />
+                    </div>
+                    <div
+                        v-if="total.appliesToFleet"
+                        class="ability-tag__boost cursor-pointer"
+                        v-tooltip="'Boosts Fleet'"
+                    >
+                        <IconShareAlt class="text-sm text-teal-900" />
+                    </div>
                 </div>
                 <div
-                    v-if="total.hasQualifiers"
-                    class="cursor-pointer"
-                    v-tooltip="
-                        `Qualifier: ${total.source.extra.abilityQualifiers}`
-                    "
+                    class="stat-total__text cursor-pointer"
+                    @click="toggleOpened"
                 >
-                    <IconExclamationTriangle class="text-sm text-orange-900" />
+                    {{ total.category }}
                 </div>
-                <div
-                    v-if="total.appliesToFleet"
-                    class="ability-tag__boost cursor-pointer"
-                    v-tooltip="'Boosts Fleet'"
-                >
-                    <IconShareAlt class="text-sm text-teal-900" />
+                <div class="flex items-center ml-auto">
+                    <div class="stat-total__amount">
+                        <template v-if="total.amountType === 'percent'">
+                            {{ `${(total.value * 100).toFixed(2)}%` }}
+                        </template>
+                        <template v-else>
+                            {{ total.value.toLocaleString() }}
+                        </template>
+                    </div>
+                    <div
+                        class="stat-total__strength text-sm"
+                        v-if="total.strength > 1"
+                    >
+                        x{{ total.strength }}
+                    </div>
                 </div>
             </div>
-            <div class="stat-total__text">
-                {{ total.category }}
-            </div>
-            <div class="flex items-center ml-auto">
-                <div class="stat-total__amount">
-                    <template v-if="total.amountType === 'percent'">
-                        {{ `${(total.value * 100).toFixed(2)}%` }}
-                    </template>
-                    <template v-else>
-                        {{ total.value.toLocaleString() }}
-                    </template>
+            <div
+                class="stat-total__description pt-1 text-gray-600"
+                v-show="opened"
+            >
+                <div class="flex">
+                    <div
+                        class="stat-total__info pr-2"
+                        v-tooltip="total.source.extra.amountDescription"
+                        v-if="
+                            total.type ===
+                            DataService.ABILITY_TYPES.EXTRA_ATTACK
+                        "
+                    >
+                        <IconQuestionCircle />
+                    </div>
+                    <div>
+                        {{ total.description }}
+                    </div>
                 </div>
-                <div
-                    class="stat-total__strength text-sm"
-                    v-if="total.strength > 1"
-                >
-                    x{{ total.strength }}
-                </div>
-            </div>
-            <div></div>
-            <div class="stat-total__description">
-                {{ total.description }}
             </div>
         </div>
     </div>
@@ -128,6 +169,7 @@ var example = {
     border-radius: 4px;
     margin-bottom: 5px;
     background: #efefef;
+    width: 215px;
 
     &--defense {
         @apply border-blue-500;
@@ -139,23 +181,6 @@ var example = {
 
     &--passive {
         @apply border-green-500;
-    }
-
-    &__description {
-        display: none;
-        position: absolute;
-        top: -100%;
-        left: 50%;
-        transform: translateX(-50%);
-        background: #333;
-        color: #fff;
-        padding: 5px;
-        border-radius: 4px;
-        z-index: 3;
-    }
-
-    &:hover &__description {
-        display: block;
     }
 }
 </style>
