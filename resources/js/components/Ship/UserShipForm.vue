c89fbdfef3c431804c618fed2b98322df8b0d5f5
Daniel Farr <daniel@aptivada.com>
2023-06-19 01:38:09 -0600

    basic OCR not great. Must train!

--- resources/js/components/Ship/UserShipForm.vue
+++ resources/js/components/Ship/UserShipForm.vue
@@ -12,6 +12,8 @@ import DataService from "../../services/DataService";
 
 import DropZoneInline from "../controls/DropZoneInline.vue";
 
+import useTesseract from "../../hooks/useTesseract";
+
 const props = defineProps({
     ship: {
         type: Object,
@@ -38,6 +40,8 @@ const props = defineProps({
 const toast = useToast();
 
 const isSaving = ref(false);
+
+const { getWorker } = useTesseract();
 
 const form = reactive({
     name: "",
@@ -97,6 +101,18 @@ const onFileUploaded = async ({ url: stat_img }) => {
         await props.onUpdate(props.userShip.id_user_ship, {
             stat_img,
         });
+    }
+
+    const worker = await getWorker();
+
+    try {
+        // const { data } = await worker.detect(stat_img);
+        const { data, ...xs } = await worker.recognize(stat_img, {
+            rectangle: { top: 103, left: 32, width: 176, height: 520 },
+        });
+        console.log("data was", data, xs);
+    } catch (err) {
+        console.error("Caught error parsing image", err);
     }
 };
 
---
+++ resources/js/hooks/useTesseract.js
@@ -0,0 +1,32 @@
+import { createWorker, PSM, OEM } from "tesseract.js";
+
+const worker = null;
+
+const createWorkerInstance = async () => {
+    const worker = await createWorker({
+        logger: (m) => console.log(m),
+    });
+
+    console.log("was worker", worker);
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    // await worker.initialize("eng", OEM.LSTM_ONLY);
+    await worker.setParameters({
+        // tessedit_pageseg_mode: PSM.SINGLE_BLOCK,
+    });
+
+    return worker;
+};
+
+const getWorker = async () => {
+    return worker || (await createWorkerInstance());
+};
+
+const useTesseract = () => {
+    return {
+        getWorker,
+    };
+};
+
+export default useTesseract;
